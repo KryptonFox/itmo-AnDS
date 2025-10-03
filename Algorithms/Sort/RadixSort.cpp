ec9ef787b1ebb1e55909cb7f3455f6eef493b4b1
KryptonFox <kirieshka0785@gmail.com>
2025-10-03 15:51:59 +0300

    Sort: Fix Radix sort

--- Algorithms/Sort/RadixSort.cpp
+++ Algorithms/Sort/RadixSort.cpp
@@ -3,32 +3,44 @@
 const int kMaxKey = 9;
 const int kCountArrLen = kMaxKey + 1;
 
-int Key(int n, int k) { return k > 0 ? (n / (10 * k)) % 10 : n % 10; }
+int Pow(int n, int power) {
+  int r = 1;
+  for (int i = 0; i < power; i++) r *= n;
+  return r;
+}
+
+int Digit(int n, int k) { return (n / Pow(10, k)) % 10; }
 
 int* CountSort(int* arr, int n, int k) {
-  int* c = new int[kCountArrLen];
-  for (int i = 0; i < kCountArrLen; i++) c[i] = 0;
+  int c[kCountArrLen];
+  for (int i = 0; i < kCountArrLen; i++) {
+    c[i] = 0;
+  };
 
   for (int i = 0; i < n; i++) {
-    c[Key(arr[i], k)] += 1;
+    c[Digit(arr[i], k)]++;
   }
 
-  for (int i = 1; i < kCountArrLen; i++) c[i] += c[i - 1];
+  for (int i = 1; i < kCountArrLen; i++) {
+    c[i] += c[i - 1];
+  };
 
-  int* sorted = new int[n + 1];
+  int* sorted = new int[n];
   for (int i = n - 1; i >= 0; i--) {
-    sorted[c[Key(arr[i], k)]] = arr[i];
-    --c[Key(arr[i], k)];
+    int d = Digit(arr[i], k);
+    sorted[c[d] - 1] = arr[i];
+    --c[d];
   }
 
-  return sorted + 1;
+  return sorted;
 }
 
 int* RadixSort(int* arr, int n, int k) {
-  for (int i = k; i > 0; i--) {
-    arr = CountSort(arr, n, k - i);
-    for (int i = 0; i < n; i++) std::cout << arr[i] << " ";
-    std::cout << std::endl;
+  int* t;
+  for (int i = 0; i < k; i++) {
+    t = CountSort(arr, n, i);
+    delete[] arr;
+    arr = t;
   }
   return arr;
 }
@@ -40,7 +52,9 @@ int main() {
   int* arr = new int[n];
   for (int i = 0; i < n; i++) std::cin >> arr[i];
 
-  int* sorted = RadixSort(arr, n, 5);
+  int* sorted = RadixSort(arr, n, 3);
 
   for (int i = 0; i < n; i++) std::cout << sorted[i] << " ";
+
+  delete[] sorted;
 }
